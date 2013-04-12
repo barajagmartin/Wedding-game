e9b6ca02c17358333dcdba26fed990047f21e867
Martin Hermansson <martinhermansson92@gmail.com>
2013-04-12 14:35:54 +0200

    Added classes for a first iteration

---
+++ src/GameModel.java
@@ -0,0 +1,19 @@
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.StateBasedGame;
+
+
+public class GameModel extends StateBasedGame {
+
+	public GameModel(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void initStatesList(GameContainer container) throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
