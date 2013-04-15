7b7f87e2208d4d90cc5a4463b6c8efe4667f99c7
Josefin Kvillert <JosKvillert@gmail.com>
2013-04-15 11:31:22 +0200

    Add method keyPressedUpdate and some variables

--- src/Controller/CharacterController.java
+++ src/Controller/CharacterController.java
@@ -1,9 +1,33 @@
 package Controller;
 
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Input;
+
+import Model.CharacterModel;
+
 public class CharacterController {
+	private CharacterModel character;
 	//determines how fast the character will move
-		private float speed = 0.2f;
+	private float speed = 0.2f;
+	private float posX;
+	private float posY;
 		
-		
-		//check which key is pressed
+	//check which key is pressed
+	public void keyPressedUpdate(GameContainer gc, int delta){
+		Input input = gc.getInput();
+		posX = character.getX();
+		posY = character.getY();
+		if(input.isKeyDown(Input.KEY_UP)){
+			posY -= speed * delta;
+		}
+		if(input.isKeyDown(Input.KEY_DOWN)){
+			posY += speed * delta;
+		}
+		if(input.isKeyDown(Input.KEY_RIGHT)){
+			posX += speed * delta;
+		}
+		if(input.isKeyDown(Input.KEY_LEFT)){
+			posX -= speed * delta;
+		}
+	}
 }
