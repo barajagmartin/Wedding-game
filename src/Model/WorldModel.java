f901009a6f57f4faefe4f02bb06fe34b201c28ab
Martin Hermansson <martinhermansson92@gmail.com>
2013-04-16 09:11:08 +0200

    Added basic JBox2D stuff to WorldModel

--- src/Model/WorldModel.java
+++ src/Model/WorldModel.java
@@ -1,16 +1,30 @@
 package Model;
 
+import org.jbox2d.common.Vec2;
+import org.jbox2d.dynamics.World;
+
 
 public class WorldModel {
 	private CharacterModel character;
+	private Vec2 gravity;
+	boolean doSleep;
+	World jBox2DWorld;
+	
 	//Kommer ha spikes, Items och CandyMonster
 	
 	
 	public WorldModel(CharacterModel character) {
 		this.character = character;
+		gravity = new Vec2(0.0f, 9.82f);
+		doSleep = true;
+		jBox2DWorld = new World(gravity, doSleep);
 	}
 	
 	public CharacterModel getCharacter(){
 		return character;
+	}
+	
+	public World getJBox2DWorld() {
+		return jBox2DWorld;
 	}
 }
