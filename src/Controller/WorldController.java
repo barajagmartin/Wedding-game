612986a6242b2775834ccae3b8f1d8422ebb8c5d
Martin Hermansson <martinhermansson92@gmail.com>
2013-04-19 14:55:21 +0200

    Refactoring done

--- src/Controller/WorldController.java
+++ src/Controller/WorldController.java
@@ -1,9 +1,41 @@
 package controller;
 
+import org.jbox2d.common.Vec2;
+
+import view.WorldView;
+
+import model.World;
+
 public class WorldController {
+	private InGameController inGameController;
 	private model.World world;
-	public WorldController(World world) {
-		this.world = world;
+	private WorldView worldView;
+	
+	public WorldController(InGameController inGameController) {
+		this.inGameController = inGameController;
+		this.world = new World(inGameController.getCharacterController().getCharacter(), 800, 500);
+		this.worldView = new WorldView(world, inGameController.getCharacterController().getCharacterView());
+	}
+	
+	public model.World getWorld() {
+		return world;
 	}
 
+	public WorldView getWorldView() {
+		return worldView;
+	}
+
+	public void moveBodyRight() {
+		//add force to move right
+	      worldView.getCharacterBody().applyLinearImpulse(new Vec2(10, 0), worldView.getCharacterBody().getPosition());
+	}
+	
+	public void moveBodyLeft() {
+		//add force to move left
+		 worldView.getCharacterBody().applyLinearImpulse(new Vec2(-10, 0), worldView.getCharacterBody().getPosition());
+	}
+	
+	public void updateSlickShape() {
+		worldView.getCharacterView().getSlickShape().setX(worldView.getCharacterBody().getPosition().x);
+		worldView.getCharacterView().getSlickShape().setY(worldView.getCharacterBody().getPosition().y);	}
 }
