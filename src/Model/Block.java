73036aaa9ab455fd71e6e278dc433d4aa1a43eeb
Sara Johansson <johanssons21@gmail.com>
2013-04-22 10:39:39 +0200

    added instance variables and constructor

--- src/Model/Block.java
+++ src/Model/Block.java
@@ -1,5 +1,18 @@
 package model;
 
 public class Block {
+	private float posX;
+	private float posY;
+	private float width;
+	private float height;
+
+	
+	public Block(final float posX, final float posY, final float width, final float height) {
+		this.posX = posX;
+		this.posY = posY;
+		this.width = width;
+		this.height = height;
+		
+	}
 
 }
