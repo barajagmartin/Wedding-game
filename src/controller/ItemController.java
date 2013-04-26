f8c4abc03eb7650a84c13e9e84266dea67134173
Sara Johansson <johanssons21@gmail.com>
2013-04-26 11:34:43 +0200

    Added the class ItemController

---
+++ src/controller/ItemController.java
@@ -0,0 +1,17 @@
+package controller;
+
+import view.ItemView;
+import model.Item;
+
+public class ItemController {
+	
+	private InGameController inGameController;
+	private Item item;
+	private ItemView itemView;
+	
+	public ItemController(InGameController inGameController) {
+		
+		
+	}
+
+}
