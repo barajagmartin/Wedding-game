7a80a94d6a7859dbfbc431a2e159239a05133928
SaraWallander <sarawallander93@gmail.com>
2013-04-26 16:24:34 +0200

    made blockMaps for items, candyMonsters and spikes

--- src/view/BlockMapView.java
+++ src/view/BlockMapView.java
@@ -10,27 +10,55 @@ import org.newdawn.slick.tiled.TiledMap;
 
 public class BlockMapView {
 	private BlockMap solidGroundMap;
+	private BlockMap candyMonsterMap;
+	private BlockMap spikesMap;
+	private BlockMap itemMap;
 	private TiledMap tiledMap;
 	
-	public BlockMapView(BlockMap solidGroundMap, TiledMap tiledMap) {
+	public BlockMapView(BlockMap solidGroundMap, BlockMap candyMonsterMap, BlockMap spikesMap, BlockMap itemMap, TiledMap tiledMap) {
 		this.solidGroundMap = solidGroundMap;
+		this.candyMonsterMap = candyMonsterMap;
+		this.spikesMap = spikesMap;
+		this.itemMap = itemMap;
 		this.tiledMap = tiledMap;
 
 		//loop through map and place out Blocks
 		for (int x = 0; x < tiledMap.getWidth(); x++) {
 			for (int y = 0; y < tiledMap.getHeight(); y++) {
 			int firstgid = tiledMap.getTileId(x, y, tiledMap.getLayerIndex("layer"));
-				String tileProperty = tiledMap.getTileProperty(firstgid, "blocked", "false"); //If there were no value it would return false
-				if (tileProperty.equals("true")) { //if the tile is solid ground, then add its properties to a Block-list
+				String tileProperty = tiledMap.getTileProperty(firstgid, "property", "nothing"); //If there were no value it would return false
+				if (tileProperty.equals("solidGround")) { //if the tile is solid ground, then add its properties to a Block-list
 					solidGroundMap.getBlockList().add(new Block(x * tiledMap.getTileWidth(),
 							y * tiledMap.getTileHeight()));
+				} else if (tileProperty.equals("candyMonster")) {
+					candyMonsterMap.getBlockList().add(new Block(x * tiledMap.getTileWidth(),
+							y * tiledMap.getTileHeight()));
+				} else if (tileProperty.equals("spikes")) {
+					spikesMap.getBlockList().add(new Block(x * tiledMap.getTileWidth(),
+							y * tiledMap.getTileHeight()));
+				} else if (tileProperty.equals("item")) {
+					itemMap.getBlockList().add(new Block(x * tiledMap.getTileWidth(),
+							y * tiledMap.getTileHeight()));
 				}
+				
 			}
 		}
 	}
 
 	public BlockMap getSolidGroundMap() {
 		return solidGroundMap;
+	}
+
+	public BlockMap getCandyMonsterMap() {
+		return candyMonsterMap;
+	}
+
+	public BlockMap getSpikesMap() {
+		return spikesMap;
+	}
+
+	public BlockMap getItemMap() {
+		return itemMap;
 	}
 
 	public TiledMap getTiledMap() {
