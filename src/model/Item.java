f9b7d37fb2e8d9df8b84247c9a7125657195efb1
Sara Johansson <johanssons21@gmail.com>
2013-04-26 10:44:02 +0200

    Added the class Item

---
+++ src/model/Item.java
@@ -0,0 +1,15 @@
+package model;
+
+public class Item {
+	
+	private int x;
+	private int y;
+	public final int CANDY_NUMBER;
+	
+	public Item(int x, int y, int candyNumber) {
+		this.x = x;
+		this.y = y;
+		this.CANDY_NUMBER = candyNumber;
+	}
+
+}
