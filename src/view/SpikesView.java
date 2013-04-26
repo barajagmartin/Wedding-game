bde5a20013368e6d1f4961e72f8ba1384ce306dd
SaraWallander <sarawallander93@gmail.com>
2013-04-26 16:49:18 +0200

    setters/getters moved things

--- src/view/SpikesView.java
+++ src/view/SpikesView.java
@@ -4,25 +4,35 @@ import model.Spikes;
 
 import org.newdawn.slick.Color;
 import org.newdawn.slick.Image;
+import org.newdawn.slick.geom.Rectangle;
 import org.newdawn.slick.geom.Shape;
 
 public class SpikesView {
 	private Image image;
 	private Shape shape;
-	public Shape getShape() {
-		return shape;
-	}
-	
+	private Spikes spikes;
+	private Color color;
+
 	public SpikesView(Spikes spikes){
 		this.spikes = spikes;
+		this.color = color.gray;
+		this.shape = new Rectangle(spikes.getX(), spikes.getY(), spikes.WIDTH, spikes.HEIGHT);
 		
 	}
 
 	public Spikes getSpikes() {
 		return spikes;
 	}
+	
+	public Shape getShape() {
+		return shape;
+	}
+	
+	public Color getColor() {
+		return color;
+	}
 
-	private Spikes spikes;
+	
 	
 	
 }
