9548b03b72fdf79a31d371aadd272235fe1bfdd3
Josefin Kvillert <JosKvillert@gmail.com>
2013-04-26 11:55:29 +0200

    Added classes CandyMonster: model, view and controller

---
+++ src/model/CandyMonster.java
@@ -0,0 +1,40 @@
+package model;
+
+import org.newdawn.slick.Color;
+
+public class CandyMonster {
+	public final int CANDY_NUMBER;
+	private int x;
+	private int y;
+	public final int WIDTH;
+	public final int HEIGHT;
+	
+	public CandyMonster(int candyNumber, int x, int y){
+		this.CANDY_NUMBER = candyNumber;
+		this.x = x;
+		this.y = y;
+		this.WIDTH = 20;
+		this.HEIGHT = 25;
+	}
+	
+	public int getX() {
+		return this.x;
+	}
+	
+	public int getY(){
+		return this.y;
+	}
+	
+	public void setX(int x){
+		this.x = x;
+	}
+	
+	public void setY(int y){
+		this.y = y;
+	}
+	
+	public int getCandyNumber() {
+		return this.CANDY_NUMBER;
+	}
+	
+}
