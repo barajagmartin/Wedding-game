f7f5e0f36fc4378576c22072d072e9380783c470
SaraWallander <sarawallander93@gmail.com>
2013-04-26 09:12:32 +0200

    edited tiles cordinates

--- src/view/BlockMapView.java
+++ src/view/BlockMapView.java
@@ -26,8 +26,8 @@ public class BlockMapView {
 			int firstgid = tiledMap.getTileId(x, y, tiledMap.getLayerIndex("two"));
 				String tileProperty = tiledMap.getTileProperty(firstgid, "blocked", "false"); //If there were no value it would return false
 				if (tileProperty.equals("true")) { //if the tile is solid ground, then add its properties to a Block-list
-					blockMap.getBlockList().add(new Block(x * tiledMap.getTileWidth(),
-							y * tiledMap.getTileHeight(),tiledMap.getTileWidth(), tiledMap.getTileHeight()));
+					blockMap.getBlockList().add(new Block((x+10) * tiledMap.getTileWidth(),
+							(y+10) * tiledMap.getTileHeight(),tiledMap.getTileWidth(), tiledMap.getTileHeight()));
 				}
 			}
 		}
