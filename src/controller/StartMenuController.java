2d911acb0a35f60c5194a83a68e19cab32614d19
SaraWallander <sarawallander93@gmail.com>
2013-05-03 14:34:56 +0200

    new classes for start menu

---
+++ src/controller/StartMenuController.java
@@ -0,0 +1,46 @@
+package controller;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.BasicGameState;
+import org.newdawn.slick.state.StateBasedGame;
+
+import view.StartMenuView;
+
+public class StartMenuController extends BasicGameState {
+
+	private StartMenuView startMenuView;
+	
+	public StartMenuController() {
+		
+	}
+	
+	@Override
+	public void init(GameContainer container, StateBasedGame game)
+			throws SlickException {
+		this.startMenuView = new StartMenuView();
+		
+	}
+
+	@Override
+	public void render(GameContainer container, StateBasedGame game, Graphics g)
+			throws SlickException {
+		this.startMenuView.render(container, game, g);
+		
+	}
+
+	@Override
+	public void update(GameContainer container, StateBasedGame game, int delta)
+			throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int getID() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
