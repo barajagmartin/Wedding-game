07cb970eb165761ab7d680eb1f70935361b23c60
Josefin Kvillert <JosKvillert@gmail.com>
2013-05-03 14:33:43 +0200

    Added class PauseView, moved stuff from PauseController to the view

--- src/controller/PauseController.java
+++ src/controller/PauseController.java
@@ -1,7 +1,8 @@
 package controller;
 
+import java.util.ArrayList;
 import model.Game;
-
+import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Input;
@@ -10,10 +11,14 @@ import org.newdawn.slick.geom.Rectangle;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 
+import view.PauseView;
+
 public class PauseController extends BasicGameState{
 	private GameContainer gc;
+	private StateBasedGame sbg;
 	private GameController gameController;
-	
+	private PauseView pauseView;
+	ArrayList<Rectangle> buttonList;
 	
 	public PauseController(GameController gameController){
 		this.gameController = gameController;
@@ -23,27 +28,16 @@ public class PauseController extends BasicGameState{
 	public void init(GameContainer gc, StateBasedGame sbg)
 			throws SlickException {
 		this.gc = gc;
+		this.sbg = sbg;
+		this.pauseView = new PauseView(gameController.getInGameController().getWorldController().getWorld().getWorldWidthPx(), 
+									gameController.getInGameController().getWorldController().getWorld().getWorldWidthPx());
 		
 	}
-	//vill först skapa en bild av spelet. Över detta menyn
+	/*Render in view*/
 	@Override
 	public void render(GameContainer gc, StateBasedGame sbg, Graphics g)
 			throws SlickException {
-		if(gc.isPaused()){
-			/*Create image of game*/
-			Rectangle rect = new Rectangle(0, 0, gameController.getInGameController().getWorldController().getWorld().getWorldWidthPx(), 
-												gameController.getInGameController().getWorldController().getWorld().getWorldHeightPx());
-												//x, y, width, height
-			
-			//gather contents from the play screen and make a new Image
-			//get graphics and render your scene in the new Image
-			//set Image as background
-			//create a fill rect for opacity and color if needed
-			
-			/*if you pause, you keep the last state id in memory and you do a transition to your paused state
-			 * if you resume you get the last state back by id and everyting "resumes" automatically without 
-			 * doing noting, the state is paused (not rendered or updated) until you go back to that state*/
-		}
+		this.pauseView.render(gc, sbg, g);
 		
 	}
 
@@ -57,8 +51,9 @@ public class PauseController extends BasicGameState{
 	@Override
 	public void keyPressed (int key, char c) {
 		if(key == Input.KEY_ESCAPE){
-			gc.setPaused(!gc.isPaused()); //Pause, unpause
+			sbg.enterState(Game.IN_GAME);
 		}
+		//Check "buttons aka Rects if pressed TODO
 	}
 
 	@Override
