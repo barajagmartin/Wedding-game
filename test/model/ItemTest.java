05309b42599b302443e16aab9eaa3ec90105c740
Sara Johansson <johanssons21@gmail.com>
2013-05-03 11:26:36 +0200

    added ItemTest

---
+++ test/model/ItemTest.java
@@ -0,0 +1,25 @@
+package model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ItemTest {
+
+	@Test
+	public void testIsPickedUp() {
+		Item item = new Item(0, 10, 1);
+		assertTrue(!item.isPickedUp());
+		item.setPickedUp(true);
+		assertTrue(item.isPickedUp());
+	}
+	
+	@Test
+	public void testIsDelivered() {
+		Item item = new Item(0, 10, 1);
+		assertTrue(!item.isDelivered());
+		item.setDelivered(true);
+		assertTrue(item.isDelivered());
+	}
+
+}
