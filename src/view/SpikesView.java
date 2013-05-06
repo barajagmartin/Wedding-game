05bd219a6c9afa03143585bc6fb4aa5fe20074f6
Sara Johansson <johanssons21@gmail.com>
2013-05-06 11:50:18 +0200

    started working on spikes collision detection, not finished

--- src/view/SpikesView.java
+++ src/view/SpikesView.java
@@ -2,22 +2,42 @@ package view;
 
 import model.Spikes;
 
+import org.jbox2d.collision.shapes.CircleShape;
+import org.jbox2d.collision.shapes.Shape;
+import org.jbox2d.dynamics.Body;
+import org.jbox2d.dynamics.BodyDef;
+import org.jbox2d.dynamics.BodyType;
+import org.jbox2d.dynamics.FixtureDef;
 import org.newdawn.slick.Color;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.geom.Circle;
-import org.newdawn.slick.geom.Shape;
+
+import utils.WorldUtils;
+
 
 public class SpikesView {
 	private Image image;
-	private Shape shape;
+	private Circle circle;
 	private Spikes spikes;
 	private Color color;
+	private Shape shape;
 
-	public SpikesView(Spikes spikes){
+	public SpikesView(Spikes spikes, WorldView worldView){
 		this.spikes = spikes;
 		this.color = color.gray;
-		this.shape = new Circle(spikes.getX(), spikes.getY(), Spikes.RADIUS);
+		this.circle = new Circle(spikes.getX(), spikes.getY(), Spikes.RADIUS);
+		this.shape = new CircleShape();
+		this.shape.m_radius = WorldUtils.pixel2Meter(Spikes.RADIUS);
+		BodyDef bodyDef = new BodyDef();
+		bodyDef.position.set(WorldUtils.pixel2Meter(spikes.getX()), WorldUtils.pixel2Meter(spikes.getY()));
+		bodyDef.type = BodyType.STATIC;
+		bodyDef.fixedRotation = true;
 		
+		FixtureDef fixtureDef = new FixtureDef();
+		fixtureDef.isSensor = true;
+		
+		Body spikesBody = worldView.getjBox2DWorld().createBody(bodyDef);
+		spikesBody.createFixture(fixtureDef);
 	}
 
 	public Spikes getSpikes() {
