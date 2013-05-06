344b3ef6833f9b39cf5e3b4dda7743e39923edfa
Sara Johansson <johanssons21@gmail.com>
2013-05-06 14:53:53 +0200

    added a spikesViewList

--- src/view/InGameView.java
+++ src/view/InGameView.java
@@ -1,14 +1,19 @@
 package view;
 
+import java.util.ArrayList;
+
 import org.newdawn.slick.Animation;
 import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.imageout.ImageOut;
 import org.newdawn.slick.state.StateBasedGame;
 
+import model.Game;
 import model.InGame;
+import model.Spikes;
 import model.StatusBar;
 
 /**
@@ -20,32 +25,43 @@ public class InGameView {
 	private StatusBarView statusBarView;
 	private CharacterView characterView;
 	private Animation nelson, blink;
+	private Graphics g;
+	private ArrayList<SpikesView> spikesViewList;
 	
-	public InGameView(InGame inGame, WorldView worldView, StatusBarView statusBarView, CharacterView characterView) {
+	public InGameView(InGame inGame, WorldView worldView, StatusBarView statusBarView, CharacterView characterView, ArrayList<SpikesView> spikesViewList) {
 		this.inGame = inGame;
 		this.worldView = worldView;
 		this.statusBarView = statusBarView;
 		this.characterView = characterView;
+		this.spikesViewList = spikesViewList;
 	}
 	
 	public void render(GameContainer gc, StateBasedGame sbg, Graphics g)
 			throws SlickException {
-		
+		this.g = g;
 		//draw character
-		g.setColor(characterView.getColor());
-		g.fill(characterView.getSlickShape());
-		Image i = new Image("pics/rainbow.jpg"); //Will be get from the Multimedia class later
-		i.draw();
+		this.g.setColor(characterView.getColor());
+		this.g.fill(characterView.getSlickShape());
+		Image background = new Image("pics/rainbow.jpg"); //Will be get from the Multimedia class later
+		background.draw();
 		worldView.getBlockMapView().getTiledMap().render(0, 0);
 		//draw candyMonsters
 		for (int j = 0; j < worldView.getCandyMonsterViewList().size(); j++) {
-			g.setColor(worldView.getCandyMonsterViewList().get(j).getColor());
-			g.fill(worldView.getCandyMonsterViewList().get(j).getShape());
+			this.g.setColor(worldView.getCandyMonsterViewList().get(j).getColor());
+			this.g.fill(worldView.getCandyMonsterViewList().get(j).getShape());
 		}	
 		//draw spikes
-				for (int j = 0; j < worldView.getSpikesViewList().size(); j++) {
-					g.setColor(worldView.getSpikesViewList().get(j).getColor());
-					g.fill(worldView.getSpikesViewList().get(j).getShape());
+				for (int j = 0; j < spikesViewList.size(); j++) {
+					this.g.setColor(spikesViewList.get(j).getColor());
+					this.g.fill(spikesViewList.get(j).getShape());
+
+					//set the color when "debugging"
+					//g.setColor(worldView.getSpikesViewList().get(j).getColor());
+					g.setColor(Color.transparent); //so it does not show the circle
+					g.fill(spikesViewList.get(j).getShape());
+//					g.drawImage(spikesViewList.get(j).getImage(), 
+//							spikesViewList.get(j).getSpikes().getX()- Spikes.RADIUS - 3, 
+//							spikesViewList.get(j).getSpikes().getY() - Spikes.RADIUS - 3);
 				}
 		
 //		Image player = new Image("pics/GulNelson.png");		
@@ -60,22 +76,32 @@ public class InGameView {
 		
 		//draw items
 		for (int j = 0; j < worldView.getItemViewList().size(); j++) {
-			g.setColor(worldView.getItemViewList().get(j).getColor());
-			g.fill(worldView.getItemViewList().get(j).getShape());
+			this.g.setColor(worldView.getItemViewList().get(j).getColor());
+			this.g.fill(worldView.getItemViewList().get(j).getShape());
 		}
 		//draw a temporary timer
-		g.drawString("Time : " + this.inGame.getTime(), 10, 25);
+		this.g.drawString("Time : " + this.inGame.getTime(), 10, 25);
 		
 		//draw status bar
-		g.drawString("LIFE", 35, 570);
 		for(int k = 0; k < characterView.getCharacter().getLife(); k++) {
 			statusBarView.getHeart()[k].draw(StatusBar.HEART_POSX[k], StatusBar.HEART_POSY);
 		}
-		g.drawString("TIME", 630, 570);
-		g.setColor(Color.darkGray);
-		g.fill(statusBarView.getFixedBar());
-		g.setColor(Color.green);
-		g.fill(statusBarView.getTimeBar());
+		this.g.setColor(Color.white);
+		this.g.drawString("LIFE", StatusBar.HEART_POSX[0] - 15, StatusBar.HEART_POSY - 20);
+		this.g.drawString("TIME", StatusBar.FIXED_BAR_POSX - 15, StatusBar.HEART_POSY - 20);
+		this.g.setColor(Color.darkGray);
+		this.g.fill(statusBarView.getFixedBar());
+		this.g.setColor(Color.green);
+		this.g.fill(statusBarView.getTimeBar());
+
+	}
+	
+	public void createPauseImage() throws SlickException{
+		//Save all graphics in an Image to create an illusion of a paused screen in PauseView
+		Image pauseImage = new Image(Game.WINDOW_WIDTH, Game.WINDOW_HEIGHT);
+		this.g.copyArea(pauseImage, 0, 0);
+		ImageOut.write(pauseImage.copy(), "pics/pauseBackground.png", false);
+		pauseImage.destroy();
 	}
 
 	
