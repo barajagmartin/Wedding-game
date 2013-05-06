e6b797206b2c4f647d402002f69d629b72e64717
SaraWallander <sarawallander93@gmail.com>
2013-05-06 14:08:39 +0200

    trying to get the score to show at the end of level

--- src/controller/InGameController.java
+++ src/controller/InGameController.java
@@ -31,6 +31,7 @@ public class InGameController extends BasicGameState {
 	private InGame inGame;
 	private InGameView inGameView;
 	private CharacterController characterController;
+	private PlayerController playerController;
 	private WorldController worldController;
 	private StatusBarController statusBarController;
 	private BlockMapController blockMapController;
@@ -75,6 +76,7 @@ public class InGameController extends BasicGameState {
 		 this.worldController = new WorldController(this);
 		 this.statusBarController = new StatusBarController(this);
 		 this.characterController = new CharacterController(this);
+		 this.playerController = new PlayerController(characterController, this);
 		 this.inGame = new InGame();
 		 this.inGameView = new InGameView(inGame, worldController.getWorldView(), statusBarController.getStatusBarView(), 
 				 characterController.getCharacterView());
@@ -158,6 +160,7 @@ public class InGameController extends BasicGameState {
 	public void checkGameOverConditions() {
 		if (this.itemController.size() == itemsDelivered) {
 			System.out.println("No more items to pick up, level cleared!");
+			this.playerController.getPlayer().setScore((int)this.inGame.getTime(), this.itemsDelivered);
 			sbg.enterState(Game.END_OF_LEVEL);
 		} else if (this.characterController.getCharacter().getLife() == 0 || this.inGame.getTime() <= 0) {
 			System.out.println("No more lives, you are dead!");
@@ -205,6 +208,11 @@ public class InGameController extends BasicGameState {
 
 	public ArrayList<SpikesController> getSpikesController() {
 		return spikeController;
+	}
+
+
+	public PlayerController getPlayerController() {
+		return playerController;
 	}
 
 }
