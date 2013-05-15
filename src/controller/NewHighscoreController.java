9780d4ae2fb51161352e43e04bfb2c68c64b0762
Sara Johansson <johanssons21@gmail.com>
2013-05-15 15:50:33 +0200

    refactored some new highscore things

--- src/controller/NewHighscoreController.java
+++ src/controller/NewHighscoreController.java
@@ -1,11 +1,15 @@
 package controller;
 
+import java.awt.Font;
+
 import model.Game;
 
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.UnicodeFont;
+import org.newdawn.slick.gui.TextField;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 import view.NewHighscoreView;
@@ -14,6 +18,8 @@ public class NewHighscoreController extends BasicGameState {
 	
 	private NewHighscoreView newHighscoreView;
 	private StateBasedGame sbg;
+	private UnicodeFont font;
+	private TextField textField;
 	
 	public NewHighscoreController() {
 		
@@ -21,9 +27,16 @@ public class NewHighscoreController extends BasicGameState {
 
 	@Override
 	public void init(GameContainer gc, StateBasedGame sbg)
-			throws SlickException {				
-		this.newHighscoreView = new NewHighscoreView();
+			throws SlickException {						
 		this.sbg = sbg;
+		this.font = new UnicodeFont(new Font("Courier", Font.BOLD, 50), 50, true, false);
+		
+		this.textField = new TextField(gc, font, Game.WINDOW_WIDTH/4, Game.WINDOW_HEIGHT/2, 300, 50);
+		this.textField.setMaxLength(8);
+		textField.setText("Enter name");
+		textField.setFocus(true);
+		this.newHighscoreView = new NewHighscoreView(textField);
+		
 	}
 
 	@Override
@@ -36,7 +49,7 @@ public class NewHighscoreController extends BasicGameState {
 	@Override
 	public void update(GameContainer gc, StateBasedGame sbg, int delta)
 			throws SlickException {
-		// TODO Auto-generated method stub
+		this.font.loadGlyphs();
 
 	}
 	
--- src/view/NewHighscoreView.java
+++ src/view/NewHighscoreView.java
@@ -8,6 +8,7 @@ import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.TrueTypeFont;
+import org.newdawn.slick.UnicodeFont;
 import org.newdawn.slick.gui.TextField;
 import org.newdawn.slick.state.StateBasedGame;
 
@@ -15,20 +16,17 @@ public class NewHighscoreView {
 	
 	private TextField textField;
 	
-	public NewHighscoreView() {
-		
+	public NewHighscoreView(TextField textField) {
+		this.textField = textField;
 		
 	}
 
 	public void render(GameContainer gc, StateBasedGame sbg, Graphics g)
 			throws SlickException {		
-		TrueTypeFont font = new TrueTypeFont(new Font("Courier", Font.BOLD, 50), false);
-		g.setFont(font);
+		//TrueTypeFont font = new TrueTypeFont(new Font("Courier", Font.BOLD, 50), false);			
 		g.drawString("New highscore!", Game.WINDOW_WIDTH/4, Game.WINDOW_HEIGHT/4);
-		this.textField = new TextField(gc, font, Game.WINDOW_WIDTH/4, Game.WINDOW_HEIGHT/2, 300, font.getHeight());
-		this.textField.setMaxLength(8);
-		textField.setText("Enter name");
 		textField.render(gc, g);
+		
 	}
 
 	public TextField getTextField() {
