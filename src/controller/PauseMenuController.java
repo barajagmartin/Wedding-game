7530bac21725337ebc66ab74215ec5d3aa55a5d3
Sara Johansson <johanssons21@gmail.com>
2013-05-16 11:46:30 +0200

    Bug fix, now the game restarts when you hit Start Game in the pause menu.

--- src/controller/PauseMenuController.java
+++ src/controller/PauseMenuController.java
@@ -89,7 +89,8 @@ public class PauseMenuController extends BasicGameState{
 						break;
 				case 3: //Enter state "Controls"
 						break;
-				case 4: gameController.getInGameController().getMusic().stop(); //Stop current thread
+				case 4: gameController.getInGameController().setPaused(false);
+						gameController.getInGameController().getMusic().stop(); //Stop current thread
 						gameController.getInGameController().getMusic().play(); //Begin new thread
 						sbg.enterState(Game.START_MENU);
 						break;
