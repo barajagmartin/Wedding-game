143e680205499e1e1ab5907b20b8fe8d04490945
SaraWallander <sarawallander93@gmail.com>
2013-05-16 10:29:16 +0200

    changed the tryToSaveScore method to saveScore, it now saves both the score and name belonging to the score to files and arrays

--- src/controller/GameController.java
+++ src/controller/GameController.java
@@ -109,42 +109,51 @@ public class GameController extends StateBasedGame {
 	 * saves your score to the file if it is high enough
 	 * @param newScore the new score you want to save
 	 */
-	public void tryToSaveScore (int newScore) {
-		if (newScore > this.scoreList[9]) {
-			this.scoreList[9] = newScore; //ändra: kolla detta innan anrop, börja loopen på 9!
-			for (int i = 8; i >= 0; i--) { //alternativt läs in en spelares namn här!
-				if (newScore > this.scoreList[i]) {
-					this.scoreList[i+1] = this.scoreList[i];
-					this.scoreList[i] = newScore;
-					if (i == 0 || this.scoreList[i-1] > newScore) {
-						break;
-					}
+	public void saveScore (int newScore, String name) { 
+		for (int i = 9; i >= 0; i--) { 
+			if (newScore > this.scoreList[i]) {
+				this.scoreList[i+1] = this.scoreList[i];
+				this.nameList[i+1] = this.nameList[i];
+				this.scoreList[i] = newScore;
+				this.nameList[i] = name;
+				if (i == 0 || this.scoreList[i-1] > newScore) {
+					break;
 				}
 			}
-			this.scoreFile.delete();
-			this.scoreFile = new File("scoreList.txt");
-			BufferedWriter outputWriter = null;
+		}
+		this.scoreFile.delete();
+		this.nameFile.delete();
+		this.scoreFile = new File("savings/scoreList.txt");
+		this.nameFile = new File("savings/nameList.txt");
+		BufferedWriter outputWriterS = null;
+		BufferedWriter outputWriterN = null;
+		try {
+			outputWriterS = new BufferedWriter(new FileWriter("savings/scoreList.txt"));
+			outputWriterN = new BufferedWriter(new FileWriter("savings/NameList.txt"));
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+		for (int j = 0; j < 10; j++) {
 			try {
-				outputWriter = new BufferedWriter(new FileWriter("scoreList.txt"));
-			} catch (IOException e1) {
-				e1.printStackTrace();
-			}
-			for (int j = 0; j < 10; j++) {
-			    try {
-					outputWriter.write(Integer.toString(this.scoreList[j]));
-					outputWriter.newLine();  
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-			try {
-				outputWriter.flush();
-				outputWriter.close();
+				outputWriterS.write(Integer.toString(this.scoreList[j]));
+				outputWriterS.newLine();
+				outputWriterN.write(this.nameList[j]);
+				outputWriterN.newLine();
+				
 			} catch (IOException e) {
 				e.printStackTrace();
-			}  
-			
+			}
 		}
+		try {
+			outputWriterS.flush();
+			outputWriterS.close();
+			outputWriterN.flush();
+			outputWriterN.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}  
+
+
 		System.out.println("new scoreList: " + Arrays.toString(scoreList));
 	}
 }
