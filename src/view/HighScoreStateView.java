c1dfa56d3978a35e9f2ae15395c9199bb6353498
Martin Hermansson <martinhermansson92@gmail.com>
2013-05-24 15:29:45 +0200

    Savings are created if there is no such file

--- src/view/HighScoreStateView.java
+++ src/view/HighScoreStateView.java
@@ -8,15 +8,10 @@ import org.newdawn.slick.Graphics;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.StateBasedGame;
 
-public class HighScoreStateView {
-	
-	public int[] scoreList;
-	public String[] nameList;
-	
-	public HighScoreStateView (int[] scoreList, String[] nameList) {
-		this.scoreList = scoreList;
-		this.nameList = nameList;
-	}
+import utils.SaveUtils;
+
+public class HighScoreStateView {	
+	public HighScoreStateView () {}
 
 	public void render(GameContainer gc, StateBasedGame sbg, Graphics g)
 			throws SlickException {
@@ -27,7 +22,8 @@ public class HighScoreStateView {
 			if (i == 9) {
 				space = "";
 			}
-			g.drawString(Integer.toString(i+1) + ". " + space + this.nameList[i] + " .................................... " + Integer.toString(this.scoreList[i]), (Game.WINDOW_WIDTH/2-200), y);
+			g.drawString(Integer.toString(i+1) + ". " + space + SaveUtils.getNameList()[i] +
+					" .................................... " + Integer.toString(SaveUtils.getScoreList()[i]), (Game.WINDOW_WIDTH/2-200), y);
 			y = y + 40;
 		}
 		
