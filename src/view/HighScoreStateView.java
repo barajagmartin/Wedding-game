c2ee33380007c9ac51da26b2bc1144feeeaf1310
Martin Hermansson <martinhermansson92@gmail.com>
2013-05-26 17:24:38 +0200

    Fixed representation of highscore list

--- src/view/HighScoreStateView.java
+++ src/view/HighScoreStateView.java
@@ -11,22 +11,38 @@ import org.newdawn.slick.state.StateBasedGame;
 
 import utils.SaveUtils;
 
-public class HighScoreStateView {	
-	public HighScoreStateView () {}
+public class HighScoreStateView {
+	private Image background;
+	
+	public HighScoreStateView () throws SlickException {
+		background = new Image("pics/highscore_background.png");
+	}
 
 	public void render(GameContainer gc, StateBasedGame sbg, Graphics g)
 			throws SlickException {
-		Image background = new Image("pics/highscore_background.png");
 		background.draw();
 		g.setColor(Color.green);
 		int y = 200;
+		StringBuilder sb = new StringBuilder();
+		
 		String space = " ";
 		for (int i = 0; i < 10; i++) {
+			sb.delete(0, sb.length());
 			if (i == 9) {
 				space = "";
 			}
-			g.drawString(Integer.toString(i+1) + ". " + space + SaveUtils.getNameList()[i] +
-					" .................................... " + Integer.toString(SaveUtils.getScoreList()[i]), (Game.WINDOW_WIDTH/2-200), y);
+			sb.append(Integer.toString(i+1)).append(". ").append(space).append(SaveUtils.getNameList()[i]);
+			if (SaveUtils.getNameList()[i].length() == 0) {
+				sb.append(".");
+			} else {
+				sb.append(" ");
+			}
+			for (int j = 0 ; j < 36 - SaveUtils.getNameList()[i].length(); j++) {
+				sb.append(".");
+			}
+			sb.append(Integer.toString(SaveUtils.getScoreList()[i]));
+			
+			g.drawString(sb.toString(), (Game.WINDOW_WIDTH/2-200), y);
 			y = y + 40;
 		}
 	}
