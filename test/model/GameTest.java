3a1bcb42a32621a06379c301c448a56a56494efd
Sara Johansson <johanssons21@gmail.com>
2013-05-26 14:16:25 +0200

    Added a test class for Game.

---
+++ test/model/GameTest.java
@@ -0,0 +1,25 @@
+package model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GameTest {
+
+	@Test
+	public void testIsSoundOn() {
+		Game game = new Game(new InGame(new Player()));
+		assertTrue(game.isSoundOn());
+		game.setSoundOn(false);
+		assertTrue(!game.isSoundOn());
+	}
+	
+	@Test
+	public void testMusicOn() {
+		Game game = new Game(new InGame(new Player()));
+		assertTrue(game.isMusicOn());
+		game.setMusicOn(false);
+		assertTrue(!game.isMusicOn());
+	}
+
+}
